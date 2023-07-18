5b9f30c5717e3304b5200eb332e342efa666ef87
ChullohChamp <basilwasonga06@gmail.com>
2023-07-18 15:12:52 -0700

    Fibonacci 98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long prev = 1, current = 2, next;
+	int count;
+
+	printf("%lu, %lu", prev, current);
+
+	for (count = 2; count < 98; count++)
+	{
+	next = prev + current;
+	printf(", %lu", next);
+	prev = current;
+	current = next;
+	}
+
+	printf("\n");
+	return (0);
+}
