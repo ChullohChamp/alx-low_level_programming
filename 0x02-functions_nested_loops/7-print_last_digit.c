b437197211684aa2ef7d2ab3536a754b3bdaf9ff
ChullohChamp <basilwasonga06@gmail.com>
2023-07-18 13:08:14 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,13 +9,15 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-	
-	{
-	n = -n;
-	}
-
 	last_digit = n % 10;
+
+	if (n < 0)
+
+	{
+
+	n = -n;
+
+	}
 
 	_putchar(last_digit + '0');
 
