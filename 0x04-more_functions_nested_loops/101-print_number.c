a73a4b9de72d39b21f2e2dd5b4e26929f358bb8e
ChullohChamp <basilwasonga06@gmail.com>
2023-07-20 15:15:46 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,19 +6,22 @@
  * @n: Represents number to be printed
  *
  */
- void print_number(int n)
+void print_number(int n)
 {
 	unsigned int num = n;
+
 	if (n < 0)
+
 	{
 		_putchar('-');
 		num = -num;
 	}
 
 	if (num > 9)
+
 	{
 	print_number(num / 10);
 	}
 
 	_putchar(num % 10 + '0');
-}	
+}
