a02688bfa7e3712046ae0313bda5735d9ccacf80
ChullohChamp <basilwasonga06@gmail.com>
2023-07-20 12:44:17 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
 /**
   * print_numbers - Print the numbers since 0 up to 9
   *
   * Return: The numbers since 0 up to 9
   */
+void _putchar(char c)
+{
+    // Implement the _putchar function as per your requirements.
+    // For simplicity, I'll use printf here.
+    printf("%c", c);
+}
 void print_numbers(void)
 {
-	int x;
-
-	for (x = 0; x <= 9; x++)
+	char numbers[] = "0123456789\n";
+	int i = 0;
+	while (numbers[i] != '\0') 
 	{
-		_putchar(x + '0');
-	}
-
-	_putchar('\n');
+	_putchar(numbers[i]);
+	i++;
+    	}
 }
-
