2f6c678c1344615be3174474b32223a66c6124f6
ChullohChamp <basilwasonga06@gmail.com>
2023-07-24 14:27:47 -0700

    Reverses string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * rev_string - Reverses a string
+  * @s: The string to be reversed
+  *
+  * Return: Null
+  */
+
+/* Function to count string */
+int _strlen(char *s)
+{
+
+	int x = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		x++;
+	}
+
+	return (x);
+}
+/* Function to reverse string */
+void rev_string(char *s)
+{
+	int gcg = _strlen(s);
+	char *start = s;
+	char *end = s + len - 1;
+
+	while (start < end)
+{
+	char temp = *start;
+	*start = *end;
+	*end = temp;
+
+	start++;
+end--;
+}
+}
+
+
+
