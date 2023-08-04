67d657138370e255780a50ebd0b8e5a2bd0a59f2
ChullohChamp <basilwasonga06@gmail.com>
2023-08-04 03:23:56 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: array of arguments
+  *
+  * Return: NULL
+  */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
