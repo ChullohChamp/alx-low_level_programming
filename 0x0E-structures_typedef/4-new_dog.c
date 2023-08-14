43e5c28cfc2ff85437e1d017546d5b8dc14b1270
ChullohChamp <basilwasonga06@gmail.com>
2023-08-14 08:41:02 -0700

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,5 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
   * new_dog - creates a new struct of type dog
   * @name: struct parameter name
@@ -9,41 +7,33 @@
   * @owner: struct parameter owner
   * Return: returns pointer to buffer of datatype dog
   */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int nlen, olen, i;
 	dog_t *doggy;
 
-	nlen = strlen(name) + 1;
-	olen = strlen(owner) + 1;
-
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
 	doggy = malloc(sizeof(dog_t));
-		if (doggy == NULL)
+	if (doggy == NULL)
 		return (NULL);
 
-	doggy->name = malloc(nlen * sizeof(char));
+	doggy->name = malloc(nlen * sizeof(doggy->name));
 	if (doggy->name == NULL)
-	{
-		free(doggy);
 		return (NULL);
-	}
-
-	strcpy(doggy->name, name);
+	for (i = 0; i < nlen; i++)
+		doggy->name[i] = name[i];
 
 	doggy->age = age;
 
-	doggy->owner = malloc(olen * sizeof(char));
-
+	doggy->owner = malloc(olen * sizeof(doggy->owner));
 	if (doggy->owner == NULL)
-	{
-		free(doggy->name);
-		free(doggy);
 		return (NULL);
-	}
-
-	strcpy(doggy->owner, owner);
-
+	for (i = 0; i < olen; i++)
+		doggy->owner[i] = owner[i];
 	return (doggy);
 }
 
