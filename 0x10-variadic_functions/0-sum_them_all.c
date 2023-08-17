369d49c136ddcc65fe8ccaa4ed6cbce3293fe549
ChullohChamp <basilwasonga06@gmail.com>
2023-08-17 12:24:03 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,14 +16,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
-	
+
 	va_start(ap, n);
-	
+
 	for (i = result = 0; i < n; i++)
 	{
 		result += va_arg(ap, int);
 	}
-	
+
 	va_end(ap);
 	return (result);
 }
