85b424be84c4d7c51348e6095ccffbb3ce36d55b
ChullohChamp <basilwasonga06@gmail.com>
2023-08-29 05:35:06 -0700

    new front

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_nodeint_end - Adds a new node at the end of a list
+ * @head: Pointer to address of current list
+ * @n: Integer to add
+ *
+ * Return: Address of the new node
+ *else  NULL if failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *current = *head;
+
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new != NULL)
+	{
+	return (NULL);
+	}
+
+	new->n = n;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	while (old->next)
+		current = current->next;
+	current->next = new;
+	return (new);
+}
+
