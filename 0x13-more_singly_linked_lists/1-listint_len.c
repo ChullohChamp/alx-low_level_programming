84b6b17083890f2f0d2d94aa429c75ffa1338a0f
ChullohChamp <basilwasonga06@gmail.com>
2023-08-29 03:20:30 -0700

    node count

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,4 +1,5 @@
-#include <lists.h>
+#include <stddef.h>
+#include "lists.h"
 
 /**
  * listint_len - Returns number of elements in a linked list
@@ -9,9 +10,9 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t x;
+	size_t count = 0;
 
-	for (x = 0; h; x++)
+	while (h != NULL)
 	{
 		h = h->next;
 		count++;
