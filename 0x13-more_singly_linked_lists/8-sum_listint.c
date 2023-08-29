d7bb3869a31de88d83e3226b897e5836b76aba5c
ChullohChamp <basilwasonga06@gmail.com>
2023-08-29 13:05:54 -0700

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -12,13 +12,10 @@ int sum_listint(listint_t *head)
 {
 	int sum = 0;
 
-	listint_t *current = head;
-
-	while (current != NULL)
+	while (head)
 	{
-		sum += current->data;
-		current = current->next;
+		sum += head->n;
+		head = head->next;
 	}
-
 	return (sum);
 }
