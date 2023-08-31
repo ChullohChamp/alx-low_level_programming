b954379b1a9d705b1f46416f9f5911c48506c1ab
ChullohChamp <basilwasonga06@gmail.com>
2023-08-31 14:08:08 -0700

    set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
 
-	return ((n >> index) & 1);
+	return ((*n |= 1 << index) ? 1 : -1);
 }
 
