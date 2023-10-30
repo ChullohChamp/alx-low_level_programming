f7f82629e4b7f727e540276d3f16f9411c793813
ChullohChamp <basilwasonga06@gmail.com>
2023-10-30 15:36:35 -0700

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -13,6 +13,9 @@ int _strlen(char *s);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_file(const char *src, const char *dest);
+void error_98(int f0, char *buffer, char *argv);
+void error_99(int f0, char *buffer, char *argv);
+void error_100(int f0, char *buffer);
 
 #endif
 
